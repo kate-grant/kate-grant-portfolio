625c335fe735e4eaac6ec4731acf6f80863c88da
Kate Grant <hello.kategrant@gmail.com>
2023-03-31 23:41:29 -0400

    fix link order

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import * as React from "react";
 const Menu = () => {
   return (
     <div>
-      <a href=''>About</a><a href=''>GitHub</a><a href=''>Work</a><br/><a href=''>Play</a><a href=''>Contact</a>
+      <a href=''>About</a><a href=''>GitHub</a><a href=''>Blog</a><br/><a href=''>Work</a><a href=''>Play</a><a href=''>Contact</a>
     </div>
   )
 }
