4cd3875a137e23b82dc5f26cc9b5ca0a617af6fc
Kate Grant <hello.kategrant@gmail.com>
2023-04-05 19:52:43 -0400

    add text styling and additional fadeIns

--- src/components/DesktopLayout.js
+++ src/components/DesktopLayout.js
@@ -17,13 +17,27 @@ const DeskTopLayout = () => {
     rootMargin: "30%",
   });
 
+  const { ref: webRef, inView: webInView} = useInView({
+    threshold: 1,
+    rootMargin: "50%",
+  });
+
+  const { ref: mobileRef, inView: mobileInView} = useInView({
+    threshold: 1,
+    rootMargin: "50%",
+  });
+
+  const { ref: openSourceRef, inView: openSourceInView} = useInView({
+    threshold: 1,
+    rootMargin: "50%",
+  });
+
   const svgData = [
     { url: 'static/Flwr_Lilac.svg', x: -200, y: 40, targetX: -10, targetY: 50, rotate: 940 },
-    { url: 'static/React_OJ.svg', x: -150, y: 0, targetX: -5, targetY: 500, rotate: -8 },
+    { url: 'static/React_Honey.svg', x: -150, y: 0, targetX: -5, targetY: 500, rotate: -8 },
     { url: 'static/diamond-fade.svg', x: -600, y: -20, targetX: -500, targetY: 1000, rotate: 540 },
     { url: 'static/TypeScript_Berry.svg', x: -150, y: 0, targetX: -50, targetY: 40, rotate: -30 },
     { url: 'static/diamond-fade.svg', x: 30, y: 0, targetX: -300, targetY: 60, rotate: 560 },
-    // { url: 'static/Flwr_Lilac.svg', x: 200, y: 50, targetX: 400, targetY: 50, rotate: 860 },
     { url: 'static/Redux_OJ.svg', x: 70, y: 80, targetX: 65, targetY: 0, rotate: 25 },
     { url: 'static/Flwr_Lilac.svg', x: 200, y: 50, targetX: 400, targetY: 50, rotate: 860 },
     { url: 'static/OCaml_Berry.svg', x: 10, y: 150, targetX: -20, targetY: 40, rotate: 35 },
@@ -39,13 +53,13 @@ const DeskTopLayout = () => {
         <h1 className={styles.h1}>KATE GRANT</h1>
         <Terminal />
         <section>
-          <h2 ref={heroRef} className={`${styles.h2} ${styles.fadeIn} ${heroInView ? styles.visible : ""}`}>I am a Brooklyn-based fullstack software engineer with a love of functional programming.</h2>
+          <h2 ref={heroRef} className={`${styles.h2} ${styles.fadeIn} ${heroInView ? styles.visible : ""}`}>I am a Brooklyn-based <span className="t">fullstack</span> software engineer with a <span className={styles.wavy}>love</span> of <span className={styles.wavy}>functional</span> programming.</h2>
           <div className={styles.animationContainer}>
             <AnimatedSVGs svgData={svgData} />
           </div>
         </section>
         <section className="skills" >
-          <h3 ref={githubRef} className={`${styles.fadeIn} ${githubInView ? styles.visible : ""}`}>Check out what I'm currently building by visiting my Github.</h3>
+          <h3 ref={githubRef} className={`${styles.fadeIn} ${githubInView ? styles.visible : ""}`}>Check out what I'm currently building by visiting my <span className={styles.wavy}>Github</span>.</h3>
         </section>
         <section className="work">
           <div>
@@ -54,18 +68,18 @@ const DeskTopLayout = () => {
 
             </div>
             <div id="project-item-container-right">
-              <ul>
+              <ul ref={webRef} className={`${styles.fadeInLeft} ${webInView ? styles.visible : ""}`}>
                 <li>Kate Grant - Portfolio</li>
                 <li>StarChart 3D</li>
                 <li>Trippn</li>
                 <li>AllMatcha</li>
               </ul>
               {/* <h3>For Mobile</h3> */}
-              <ul>
+              <ul ref={mobileRef} className={`${styles.fadeInLeft} ${mobileInView ? styles.visible : ""}`}>
                 <li>Color Closet</li>
               </ul>
               {/* <h3>Open Source Contributions</h3> */}
-              <ul>
+              <ul ref={openSourceRef} className={`${styles.fadeInLeft} ${openSourceInView ? styles.visible : ""}`}>
                 <li>Three.js</li>
                 <li>P5.js</li>
                 <li>Tablecloth</li>
