0048b12c00cfe72afd43e5d67ef23297a471adbf
Kate Grant <hello.kategrant@gmail.com>
2023-04-21 22:47:24 -0400

    added links

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,11 @@
 import * as React from "react";
+import {Link} from "gatsby";
 import * as styles from "../components/Menu.module.css"
 
 const Menu = () => {
   return (
     <div className={styles.menu}>
-      <a className={styles.menuLink} href=''>About</a><a className={styles.menuLink} href=''>GitHub</a><a className={styles.menuLink} href=''>Work</a><a className={styles.menuLink} href=''>Play</a><a className={styles.menuLink} href=''>Blog</a><a className={styles.menuLink} href=''>Contact</a>
+      <Link className={styles.menuLink} to='/#about'>About</Link><a className={styles.menuLink} href='https://github.com/kate-grant' target="_blank">GitHub</a><Link className={styles.menuLink} to='/#work'>Work</Link><Link className={styles.menuLink} to=''>Play</Link><Link className={styles.menuLink} to='/blog'>Blog</Link><Link className={styles.menuLink} to='/#contact'>Contact</Link>
     </div>
   )
 }
