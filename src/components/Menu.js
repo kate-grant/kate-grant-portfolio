a9db258ce9128c5ecf84c680167dd06ad84e3822
Kate Grant <hello.kategrant@gmail.com>
2023-06-02 23:22:46 -0400

    fix play link in menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ import * as styles from "../components/Menu.module.css"
 const Menu = () => {
   return (
     <div className={styles.menu}>
-      <Link className={styles.menuLink} to='/#about'>About</Link><a className={styles.menuLink} href='https://github.com/kate-grant' target="_blank">GitHub</a><Link className={styles.menuLink} to='/#work'>Work</Link><Link className={styles.menuLink} to='#play'>Play</Link><Link className={styles.menuLink} to='/blog'>Blog</Link><Link className={styles.menuLink} to='/#contact'>Contact</Link>
+      <Link className={styles.menuLink} to='/#about'>About</Link><a className={styles.menuLink} href='https://github.com/kate-grant' target="_blank">GitHub</a><Link className={styles.menuLink} to='/#work'>Work</Link><Link className={styles.menuLink} to='/#play'>Play</Link><Link className={styles.menuLink} to='/blog'>Blog</Link><Link className={styles.menuLink} to='/#contact'>Contact</Link>
     </div>
   )
 }
